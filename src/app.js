1557d52ab668ee39c1d82ced4691a12f12b8c4a0
Kat <katdemey@outlook.com>
2023-01-18 15:52:44 +0000

    trying to get thiings to display in a map

--- src/app.js
+++ src/app.js
@@ -1,12 +1,68 @@
+import React from 'react'
 import Header from './components/header'
 
 import initialEmails from './data/emails'
 
+import { useState } from 'react'
 import './styles/app.css'
+
 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+  //make a const/ useState that will call the emails from emails.js
+  const [emails, setEmails] = useState(initialEmails);
+
+  //create list: can use map instead of for each to create the items
+  const renderEmailsInMain = (emails) => {
+
+    { emails.map((email) => {
+        return (
+          <li className="email" key={email.id}>
+            <div className="select">
+              <input
+                className="select-checkbox"
+                type="checkbox" />
+            </div>
+            <div className="star">
+              <input
+                className="star-checkbox"
+                type="checkbox"
+              />
+            </div>
+            <div className="sender"  key={email.sender}></div>
+            <div className="title" key={email.title}></div>
+          </li>
+        )
+      });
+    }
+
+    // initialEmails(emails.map((email) => {
+    //   return (
+    //     <div>
+    //       <li className="email" key= {email.id}>
+    //         <div className="select">
+    //           <input
+    //             className="select-checkbox"
+    //             type="checkbox" />
+    //         </div>
+    //         <div className="star">
+    //           <input
+    //             className="star-checkbox"
+    //             type="checkbox"
+    //           />
+    //         </div>
+    //         <div className="sender">
+
+    //         </div>
+    //         <div className="title"> {emails.title}</div>
+    //       </li>
+    //     </div>
+    //   )
+    // })
+    // );
+    // setEmails(initialEmails)
+  };
 
   return (
     <div className="app">
@@ -15,14 +71,14 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+          // onClick={() => {}}
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+          // onClick={() => {}}
           >
             <span className="label">Starred</span>
             <span className="count">?</span>
@@ -34,12 +90,12 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+            // onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{renderEmailsInMain()}</main>
     </div>
   )
 }
