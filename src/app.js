8a08fe4301f7ec960db80d1d112ebfe99ff62c19
pooja <dhanwade.pooja2688@gmail.com>
2023-01-18 18:26:29 +0000

    basic working

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
-import Header from './components/header'
+import { useState } from "react";
+import Header from "./components/header";
 
-import initialEmails from './data/emails'
+import initialEmails from "./data/emails";
 
-import './styles/app.css'
+import "./styles/app.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  console.log(initialEmails);
+  const [emailsState, setEmailState] = useState(initialEmails);
+  const [star, setstar] = useState(0);
+
+  const countStarred = () => {
+    console.log(emailsState);
+    let currentStarred = 0;
+    emailsState.forEach(email => {
+      if (email.starred) {
+        currentStarred++;
+      }
+    })
+      
+    setstar(currentStarred);
+  };
+
+const unreadEmails = emailsState.filter(function (email){
+  return email.read === false
+})
+    const [unreadEmailState, setunreadEmailState] = useState(unreadEmails)
+
+  // countStarred()
 
   return (
     <div className="app">
@@ -18,14 +40,14 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{emailsState.length}</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+            //  onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{star}</span>
           </li>
 
           <li className="item toggle">
@@ -39,9 +61,32 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {emailsState.map((thisEmail, index) => (
+          <li className="email" key={index} >
+           {/* if(thisEmail.read === true){
+             
+          } } */}
+            <div className="select">
+              <input className="select-checkbox" type="checkbox" />
+            </div>
+            <div className="star">
+              <input
+                className="star-checkbox"
+                type="checkbox"
+                onChange={() => {
+                  thisEmail.starred = !thisEmail.starred;
+                  countStarred();
+                }}
+              />
+            </div>
+            <div className="sender">{thisEmail.sender}</div>
+            <div className="title">{thisEmail.title}</div>
+          </li>
+        ))}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
