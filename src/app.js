73d1a7db19555bbf365407c50f972a80081dd3aa
Manoj kumar <manoj.bhoosani@gmail.com>
2023-01-18 19:21:08 +0100

    corde done

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import Header from "./components/header";
 import initialEmails from "./data/emails";
 
 import "./styles/app.css";
-
+ 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails);
@@ -14,9 +14,13 @@ function App() {
   const [emails, setEmails] = useState(initialEmails);
 
   //create list: can use map instead of for each to create the items
-  // const renderEmailsInMain = (emails) => {
 
-  // };
+//   const toggleRead =  (email) => {
+//     if (updatedStatus === email) {
+//       return {...updatedStatus, read: !updatedStatus.read}}
+// return updatedStatus
+//   }
+
 
   return (
     <div className="app">
@@ -54,7 +58,20 @@ function App() {
           return (
             <li className="email" key={email.id}>
               <div className="select">
-                <input className="select-checkbox" type="checkbox" />
+                <input className="select-checkbox" type="checkbox"  />
+                {/* // working on extension 1 it should be inside the input tag
+                // onChange={() => {
+                //   console.log("checked")
+                //   const updatedRead = emails.map((updatedStatus) => {
+                //        if (updatedStatus === email.id) {
+                //           return {...initialEmails,read : !initialEmails.read}
+                //         }
+                //     return updatedStatus
+                //         })
+                //         setEmails(updatedRead)
+                //         // console.log(updatedRead)
+                // }} */}
+                
               </div>
               <div className="star">
                 <input className="star-checkbox" type="checkbox" />
