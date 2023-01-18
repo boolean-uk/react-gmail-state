cdf4e70f582f14ad70cc6afc4904f2b347d3458a
Manoj kumar <manoj.bhoosani@gmail.com>
2023-01-18 18:20:10 +0100

    getting the emails but not the text

--- src/app.js
+++ src/app.js
@@ -1,68 +1,21 @@
-import React from 'react'
-import Header from './components/header'
+import React from "react";
+import Header from "./components/header";
 
-import initialEmails from './data/emails'
+import initialEmails from "./data/emails";
 
-import { useState } from 'react'
-import './styles/app.css'
-
+import { useState } from "react";
+import "./styles/app.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  console.log(initialEmails);
   //make a const/ useState that will call the emails from emails.js
   const [emails, setEmails] = useState(initialEmails);
 
   //create list: can use map instead of for each to create the items
-  const renderEmailsInMain = (emails) => {
+  // const renderEmailsInMain = (emails) => {
 
-    { emails.map((email) => {
-        return (
-          <li className="email" key={email.id}>
-            <div className="select">
-              <input
-                className="select-checkbox"
-                type="checkbox" />
-            </div>
-            <div className="star">
-              <input
-                className="star-checkbox"
-                type="checkbox"
-              />
-            </div>
-            <div className="sender"  key={email.sender}></div>
-            <div className="title" key={email.title}></div>
-          </li>
-        )
-      });
-    }
-
-    // initialEmails(emails.map((email) => {
-    //   return (
-    //     <div>
-    //       <li className="email" key= {email.id}>
-    //         <div className="select">
-    //           <input
-    //             className="select-checkbox"
-    //             type="checkbox" />
-    //         </div>
-    //         <div className="star">
-    //           <input
-    //             className="star-checkbox"
-    //             type="checkbox"
-    //           />
-    //         </div>
-    //         <div className="sender">
-
-    //         </div>
-    //         <div className="title"> {emails.title}</div>
-    //       </li>
-    //     </div>
-    //   )
-    // })
-    // );
-    // setEmails(initialEmails)
-  };
+  // };
 
   return (
     <div className="app">
@@ -71,14 +24,14 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-          // onClick={() => {}}
+            // onClick={() => {}}
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
           <li
             className="item"
-          // onClick={() => {}}
+            // onClick={() => {}}
           >
             <span className="label">Starred</span>
             <span className="count">?</span>
@@ -90,14 +43,29 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-            // onChange={() => {}}
+              // onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{renderEmailsInMain()}</main>
+      <main className="emails">
+        {emails.map((email) => {
+          return (
+            <li className="email" key={email.id}>
+              <div className="select">
+                <input className="select-checkbox" type="checkbox" />
+              </div>
+              <div className="star">
+                <input className="star-checkbox" type="checkbox" />
+              </div>
+              <div className="sender" key={email.sender}></div>
+              <div className="title" key={email.title}></div>
+            </li>
+          );
+        })}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
