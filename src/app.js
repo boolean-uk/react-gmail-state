198eb8b5ba0922b8d819bc3741b055dfc775db21
Ntina B <n_bekiari@outlook.com>
2023-08-17 19:55:45 +0300

    ok .

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
+import React, { useState } from 'react'
 import Header from './components/header'
-
 import initialEmails from './data/emails'
-
 import './styles/app.css'
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+
+  const [emails, setEmails] = useState(initialEmails);
+
+  const [currentTab, setCurrentTab] = useState("inbox");
+
+  const [hideRead, setHideRead] = useState(false);
+
+  function toggleRead(emailId) {
+    setEmails((prevEmails) =>
+      prevEmails.map((email) =>
+        email.id === emailId ? { ...email, read: !email.read } : email
+      )
+    );
+  }
+
+  function toggleStar(emailId) {
+    setEmails((prevEmails) =>
+      prevEmails.map((email) =>
+        email.id === emailId ? { ...email, starred: !email.starred } : email
+      )
+    );
+  }
+
+  const filteredEmails = (
+    currentTab === "inbox"
+      ? emails.filter((email) => !email.starred)
+      : emails.filter((email) => email.starred)
+  ).filter((email) => !hideRead || !email.read);
 
   return (
     <div className="app">
@@ -14,34 +39,65 @@ function App() {
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
-            className="item active"
-            // onClick={() => {}}
+            className={`item ${currentTab === "inbox" ? "active" : ""}`}
+            onClick={() => setCurrentTab("inbox")}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">
+              {emails.filter((email) => !email.starred).length}
+            </span>
           </li>
           <li
-            className="item"
-            // onClick={() => {}}
+            className={`item ${currentTab === "starred" ? "active" : ""}`}
+            onClick={() => setCurrentTab("starred")}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">
+              {emails.filter((email) => email.starred).length}
+            </span>
           </li>
-
           <li className="item toggle">
             <label for="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              checked={hideRead}
+              onChange={() => setHideRead(!hideRead)}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul className="email-list">
+          {filteredEmails.map((email) => (
+            <li
+              key={email.id}
+              className={`email ${email.read ? "read" : "unread"}`}
+            >
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                  checked={email.read}
+                  onChange={() => toggleRead(email.id)}
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred}
+                  onChange={() => toggleStar(email.id)}
+                />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header.js
+++ src/components/header.js
@@ -7,13 +7,12 @@ function Header() {
         <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
           <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
         </svg>
-
         <img
-          src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+          src="https://www.pngkey.com/png/detail/15-150195_gmail-comments-google-mail-logo-black-and-white.png"
+          width= '25px'
           alt="gmail logo"
         />
       </div>
-
       <div className="search">
         <input className="search-bar" placeholder="Search mail" />
       </div>
@@ -21,4 +20,4 @@ function Header() {
   )
 }
 
-export default Header
+export default Header;
