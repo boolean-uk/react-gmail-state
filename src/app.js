14aafdbd1aee27d814bbd5c13da70156538241a1
Ävadakedavr0 <dylandd4@hotmail.be>
2023-08-17 12:37:28 +0200

    core

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
-import Header from './components/header'
+import React, {useState} from 'react'
 
+import Header from './components/header'
 import initialEmails from './data/emails'
 
 import './styles/app.css'
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails] = useState(initialEmails)
+  // count unread emails
+  let unreadCounter = 0
+  let starredCounter = 0
+  
+  for (let index = 0; index < emails.length; index++) {
+      if (!emails[index].read) {
+          unreadCounter++
+      }
+  
+      if (emails[index].starred) {
+          starredCounter++
+      }
+  }
 
   return (
     <div className="app">
@@ -18,14 +32,14 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{unreadCounter}</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starredCounter}</span>
           </li>
 
           <li className="item toggle">
@@ -39,7 +53,25 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{
+        emails.map( email => 
+          (
+            //is it oke to use ternary operation on this or do you prefer I write the if else?
+          <li key={email.id} className={`email ${email.read ? 'read' : 'unread'}`}>
+            <div className="select">
+              <input className="select-checkbox" type="checkbox"/>
+              </div>
+              <div className="star">
+                <input className="star-checkbox" type="checkbox" checked ={email.starred}/>
+                </div>
+                <div className="sender">{email.sender}</div>
+                <div className="title">{email.title}</div>
+                </li>
+          )
+
+        )
+      }
+      </main>
     </div>
   )
 }
