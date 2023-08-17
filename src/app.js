5ced78e9544e17d0abb71b57341a7d86c283b155
ks46 <konastaf46@gmail.com>
2023-08-17 13:18:06 +0300

    refactored list of emails rendering

--- src/app.js
+++ src/app.js
@@ -9,6 +9,32 @@ function App() {
   // Use initialEmails for state
   const [emailsList, setEmailsList] = useState(initialEmails)
 
+  const toggleRead = (emailId) => {
+    // TODO: Create a toggleRead function that updates the target email's read property in state, when a user clicks on the checkbox
+    console.log(`emailId: ${emailId}`)
+  }
+
+  const emailItems = emailsList.map(email => 
+    <li className={`email ${email.read ? "read" : "unread"}`} key={email.id}>
+      <div className="select">
+        <input
+          className="select-checkbox"
+          type="checkbox"
+          onChange={() => {toggleRead(email.id)}}
+        />
+      </div>
+      <div className="star">
+        <input className="star-checkbox" type="checkbox"/>
+      </div>
+      <div className="sender">
+        {email.sender}
+      </div>
+      <div className="title">
+        {email.title}
+      </div>
+    </li>
+  )
+
   return (
     <div className="app">
       <Header />
@@ -42,24 +68,7 @@ function App() {
       </nav>
       <main className="emails">{
         /* Render a list of emails here, according to email-template.html */
-        <ul>{
-          emailsList.map((email, key) =>
-            <li className={`email ${email.read ? "read" : "unread"}`}>
-              <div className="select">
-                <input className="select-checkbox" type="checkbox"/>
-              </div>
-              <div className="star">
-                <input className="star-checkbox" type="checkbox"/>
-              </div>
-              <div className="sender">
-                {email.sender}
-              </div>
-              <div className="title">
-                {email.title}
-              </div>
-            </li>
-          )
-        }</ul>
+        <ul>{ emailItems }</ul>
       }</main>
     </div>
   )
