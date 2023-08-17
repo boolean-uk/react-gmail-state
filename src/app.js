6b7e8889613ee3745c9cd43847b0a22ca5a14489
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-08-17 14:29:03 +0200

    fixed an issue with the checkbox element

--- src/app.js
+++ src/app.js
@@ -1,47 +1,71 @@
-import Header from './components/header'
+import Header from "./components/header.js";
 
-import initialEmails from './data/emails'
+import initialEmails from "./data/emails.js";
 
-import './styles/app.css'
+import { useState } from "react";
+
+import "./styles/app.css";
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+    // Use initialEmails for state
+    // console.log(initialEmails);
+    const [emails, setEmails] = useState(initialEmails);
+    console.log(emails);
+    const toggleFunction = (emailId) => {
 
-  return (
-    <div className="app">
-      <Header />
-      <nav className="left-menu">
-        <ul className="inbox-list">
-          <li
-            className="item active"
-            // onClick={() => {}}
-          >
-            <span className="label">Inbox</span>
-            <span className="count">?</span>
-          </li>
-          <li
-            className="item"
-            // onClick={() => {}}
-          >
-            <span className="label">Starred</span>
-            <span className="count">?</span>
-          </li>
+    }
+    return (
+        <div className="app">
+            <Header />
+            <nav className="left-menu">
+                <ul className="inbox-list">
+                    <li
+                        className="item active"
+                        // onClick={() => {}}
+                    >
+                        <span className="label">Inbox</span>
+                        <span className="count">?</span>
+                    </li>
+                    <li
+                        className="item"
+                        // onClick={() => {}}
+                    >
+                        <span className="label">Starred</span>
+                        <span className="count">?</span>
+                    </li>
 
-          <li className="item toggle">
-            <label for="hide-read">Hide read</label>
-            <input
-              id="hide-read"
-              type="checkbox"
-              checked={false}
-              // onChange={() => {}}
-            />
-          </li>
-        </ul>
-      </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
-    </div>
-  )
+                    <li className="item toggle">
+                        <label for="hide-read">Hide read</label>
+                        <input
+                            id="hide-read"
+                            type="checkbox"
+                            checked={false}
+                            // onChange={() => {}}
+                        />
+                    </li>
+                </ul>
+            </nav>
+            <main className="emails">
+                {emails.map((email,index) => (
+                    <li  key={index} className={`email ${email.read ? "read" : "unread"}`}>
+                        <div className="select">
+                            <input
+                                className="select-checkbox"
+                                type="checkbox"
+                                checked={email.read}
+                                onChange={() => toggleFunction(email.id)}
+                            />
+                        </div>
+                        <div className="star">
+                            <input className="star-checkbox" type="checkbox" />
+                        </div>
+                        <div className="sender">{email.sender}</div>
+                        <div className="title">{email.title}</div>
+                    </li>
+                ))}
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
