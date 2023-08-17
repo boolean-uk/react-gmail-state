c1ff3fc6ede332c909ded6aa93fe447af8f296b2
iliask796 <iliaskal7@gmail.com>
2023-08-17 13:18:45 +0300

    Finished Core Criteria

--- src/app.js
+++ src/app.js
@@ -4,9 +4,31 @@ import initialEmails from './data/emails'
 
 import './styles/app.css'
 
+import { useState } from 'react'
+
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+  //Get the initail email list
+  const [emailList, updateEmailList] = useState(initialEmails)
+  //Get the number of initial unread emails 
+  const [unreadCounter, updateUnreadCounter] = useState(() => {
+    let counter = 0
+    for (let i = 0; i < emailList.length; i++) {
+      if (emailList[i].read === false){
+        counter++
+      }
+    }
+    return counter
+  })
+  //Get the number of initial starred emails
+  const [starCounter, updateStarCounter] = useState(() => {
+    let counter = 0
+    for (let i = 0; i < emailList.length; i++) {
+      if (emailList[i].starred === true){
+        counter++
+      }
+    }
+    return counter
+  })
 
   return (
     <div className="app">
@@ -18,14 +40,14 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{unreadCounter}</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starCounter}</span>
           </li>
 
           <li className="item toggle">
@@ -39,7 +61,28 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul>
+          {emailList.map((myEmail) => {
+            let isRead = 'email unread'
+            if (myEmail.read) {
+              isRead = 'email read'
+            }
+            return(
+              <li className = {isRead}>
+                <div className='select'>
+                  <input className="select-checkbox" type="checkbox" defaultChecked={myEmail.read}></input>
+                </div>
+                <div className='star'>
+                  <input className='star-checkbox' type="checkbox" defaultChecked={myEmail.starred}></input>
+                </div>
+                <div className='sender'>{myEmail.sender}</div>
+                <div className='title'>{myEmail.title}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
     </div>
   )
 }
