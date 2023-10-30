0dc6eca546db675e7184d307828057849a85656b
callumhayden <callum.hayden@gmail.com>
2023-10-30 15:10:39 +0000

    first section added, emails have appeared

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import Header from './components/header'
 import initialEmails from './data/emails'
+
 
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+
+  const [emails, setEmails] = useState(initialEmails)
 
   return (
     <div className="app">
@@ -14,14 +18,14 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+          // onClick={() => {}}
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+          // onClick={() => {}}
           >
             <span className="label">Starred</span>
             <span className="count">?</span>
@@ -33,12 +37,35 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+            // onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      
+      <main className="emails">
+        {emails.map((email) => (<li
+          key={email.id}
+          className={`email ${email.read ? "read" : "unread"}`}
+        >
+          <div className='select'>
+            <input
+              className="select-checkbox"
+              type="checkbox"
+              checked={email.read}
+            />
+          </div>
+          <div className='star'>
+            <input
+              className="star-checkbox"
+              type="checkbox"
+              checked={email.starred}
+            />
+          </div>
+          <div className='sender'>{email.sender}</div>
+          <div className='title'>{email.title}</div>
+        </li>))}
+        </main>
     </div>
   )
 }
