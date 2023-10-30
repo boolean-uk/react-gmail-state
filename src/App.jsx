1624b1d444d818a924809178ad3dc2fa9d4ca1c5
Satoki <satokiito@gmail.com>
2023-10-30 14:49:52 +0000

    core - initial emails rendered on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from './components/header'
 import initialEmails from './data/emails'
+import { useState } from 'react'
 
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+
+  const [emails, setEmails] = useState(initialEmails)
 
   return (
     <div className="app">
@@ -38,7 +41,22 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul>
+          {emails.map((email, index) => 
+          <li key={index} className='email'>
+            <div className="select">
+              <input className="select-checkbox" type="checkbox"/>
+            </div>
+            <div className="star">
+              <input className="star-checkbox" type="checkbox"/>
+            </div>
+            <div className="sender">{email.sender}</div>
+            <div className="title">{email.title}</div>
+          </li>
+          )}
+        </ul>
+    </main>
     </div>
   )
 }
