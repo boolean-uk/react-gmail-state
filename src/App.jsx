70973344cafef71e1615283fbab8fcc1eaa8a193
TomEastwood <t.eastwood99@gmail.com>
2023-10-30 16:32:34 +0000

    core completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import Header from './components/header'
 import initialEmails from './data/emails'
+import { useState } from 'react'
 
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails);
+  const [readEmails, setReadEmails] = useState();
+  const [starredEmails, setStarredEmails] = useState();
+
+  const renderEmails = emails.map((email) => {
+    return (
+      <div key ={email.id}>
+        <li className="email">
+          <div className="select">
+            <input
+              className="select-checkbox"
+              type="checkbox" />
+          </div>
+          <div className="star">
+            <input
+              className="star-checkbox"
+              type="checkbox"
+            />
+          </div>
+          <div className="sender">
+            {email.sender}
+          </div>
+          <div className="title">
+            {email.title}
+          </div>
+        </li>
+      </div>
+    )
+  })
+
+  const 
 
   return (
     <div className="app">
@@ -14,31 +45,31 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+          // onClick={() => {}}
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+          // onClick={() => {}}
           >
             <span className="label">Starred</span>
             <span className="count">?</span>
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+            onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{renderEmails}</main>
     </div>
   )
 }
