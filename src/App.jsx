e7b5d560bce585000eb15803e8d5f44ad1734af7
Rebecca Noy <rebecca.noy@gmx.de>
2023-10-31 13:54:54 +0100

    added commentary pertaining to Extension 1 toggle Read

--- src/App.jsx
+++ src/App.jsx
@@ -112,6 +112,7 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={hideRead}
+              // I'm not sure whether we're really supposed to toggle – setting it to "read = true" makes more sense – but here's the requested implementation of toggling
               onClick={() => setHideRead(!hideRead)}
             />
           </li>
