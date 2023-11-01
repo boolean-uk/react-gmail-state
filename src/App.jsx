b57b56c4e402a8abe665d15c1f7a2e019c5a423c
aayushlama4008 <aayushlama4008@gmail.com>
2023-11-01 11:01:03 +0100

    corrected the path of Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import initialEmails from './data/emails'
 
 
@@ -11,8 +11,14 @@ function App() {
   console.log(emails)
 
 
-const starredEmail = () => emails.filter(email =>email.starred)
+const starredEmail = () => emails.filter(email =>!email.starred)
 const readEmail = () => emails.filter(email => email.read)
+
+function toggleRead(email){
+  email.read = !email.read
+
+ setEmails = ([...emails])
+}
   return (
     <div className="app">
       <Header />
@@ -20,14 +26,14 @@ const readEmail = () => emails.filter(email => email.read)
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+           
           >
             <span className="label">Inbox</span>
             <span className="count">{readEmail().length}</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+           
           >
             <span className="label">Starred</span>
             <span className="count">{starredEmail().length}</span>
@@ -48,10 +54,10 @@ const readEmail = () => emails.filter(email => email.read)
         <ul>{emails.map(email =>
           <li key={email.id} className="email">
             <div className="select">
-              <input type="checkbox" className="select-checkbox" checked={email.read} />
+              <input type="checkbox" className="select-checkbox" checked={email.read}  onChange={() => {toggleRead(email)}}/>
             </div>
             <div className="star">
-              <input type="checkbox" className="star-checkbox" checked={email.starred}/>
+              <input type="checkbox" className="star-checkbox" checked={!email.starred}/>
             </div>
             <div className="sender">{email.sender}</div>
             <div className="title">{email.title}</div>
