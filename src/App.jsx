96610de7acc6f096d93c85af10f8782e5e097d39
Loza_TM <lozatmamo@gmail.com>
2023-11-01 21:45:52 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Header from './components/header'
+import Header from './components/Header'
 import initialEmails from './data/emails'
 import './styles/App.css'
 
