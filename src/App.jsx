364a8c6acf3add673df23985356ae18952c2931c
Loza_TM <lozatmamo@gmail.com>
2023-11-01 21:42:37 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import React, { useState } from 'react';
 import Header from './components/header'
 import initialEmails from './data/emails'
-
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
+  const [emails, setEmails] = useState(initialEmails);
+  let filteredEmails = emails;
   console.log(initialEmails)
 
   return (
@@ -17,28 +19,50 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">3</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">2</span>
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label for = "hide-read" >Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul>{filteredEmails.map(email =>
+         <li key={email.id} className="email">
+          <div className="select">
+          <input
+          className="select-checkbox"
+          type="checkbox"
+          checked={email.read}
+          />
+          </div>
+
+          <div className="star">
+          <input
+          className="star-checkbox"
+          type="checkbox"
+          checked={email.starred}
+          />
+
+          </div>
+          <div className="sender">{email.sender}</div>
+          <div className="title">{email.title}</div>
+         </li>
+         )}
+        </ul>
+      </main>
     </div>
   )
 }
