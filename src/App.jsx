a5df653cefb4347f6ec4e54ef1bf4543b3b4b03a
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-23 11:48:41 +0100

    finished core

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Header from './components/Header'
-import initialEmails from './data/emails'
-
-import './styles/App.css'
+import Header from "./components/Header";
+import initialEmails from "./data/emails";
+import { useState } from "react";
+import "./styles/App.css";
 
 function App() {
+  const [emails, setEmails] = useState(initialEmails);
   // Use initialEmails for state
-  console.log(initialEmails)
+  console.log(initialEmails);
 
   return (
     <div className="app">
@@ -17,14 +18,14 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{emails.length}</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">0</span>
           </li>
 
           <li className="item toggle">
@@ -38,9 +39,28 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {/* Render a list of emails here */}
+        <ul className="email-list">
+          {emails.map((email, index) => (
+            <li
+              className={`email ${email.read ? "read" : "unread"}`}
+              key={index}
+            >
+              <div className="select">
+                <input className="select-checkbox" type="checkbox" />
+              </div>
+              <div className="star">
+                <input className="star-checkbox" type="checkbox" />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
