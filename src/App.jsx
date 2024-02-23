2063c48d5c5463f86e1f2cc84e86709b38b50042
Mårten Elfving <marten.elving@gmail.com>
2024-02-23 11:02:59 +0100

    Core+ext

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
-import Header from './components/Header'
-import initialEmails from './data/emails'
+import Header from "./components/Header";
+import initialEmails from "./data/emails";
+import { useState } from "react";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  console.log(initialEmails);
+
+  const [emails, setEmails] = useState(initialEmails);
+  const [hideRead, setHideRead] = useState(false);
+  const [currentTab, setCurrentTab] = useState("Inbox");
+
+  const starredMail = () =>
+    emails.filter((email) => {
+      return email.starred === true;
+    });
+
+  function updateRead(mailToUpdate) {
+    const updatedEmails = emails.map((email) => {
+      if (mailToUpdate.id === email.id) {
+        return { ...email, read: !email.read };
+      } else {
+        return email;
+      }
+    });
+    setEmails(updatedEmails);
+  }
+
+  function updateStarred(mailToUpdate) {
+    const updatedEmails = emails.map((email) => {
+      if (mailToUpdate.id === email.id) {
+        return { ...email, starred: !email.starred };
+      } else {
+        return email;
+      }
+    });
+    setEmails(updatedEmails);
+  }
+
+  function getEmails() {
+    let emailsToRender = emails;
+    if (hideRead) {
+      emailsToRender = emailsToRender.filter((email) => email.read === false);
+    }
+
+    if (currentTab === "Starred") {
+      console.log("currentTab", currentTab, currentTab === "Starred");
+      emailsToRender = emailsToRender.filter((email) => {
+        console.log(email.starred);
+        return email.starred;
+      });
+    }
+    return emailsToRender;
+  }
 
   return (
     <div className="app">
@@ -13,34 +61,65 @@ function App() {
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
-            className="item active"
-            // onClick={() => {}}
+            className={"item " + (currentTab === "Inbox" ? "active" : "")}
+            onClick={() => {
+              setCurrentTab("Inbox");
+            }}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{emails.length}</span>
           </li>
           <li
-            className="item"
-            // onClick={() => {}}
+            className={"item " + (currentTab === "Starred" ? "active" : "")}
+            onClick={() => {
+              setCurrentTab("Starred");
+            }}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starredMail().length}</span>
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              checked={hideRead}
+              onChange={() => {
+                setHideRead(!hideRead);
+              }}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {getEmails().map((email) => {
+          return (
+            <div key={email.id} className="email">
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                  checked={email.read}
+                  onChange={() => updateRead(email)}
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred}
+                  onChange={() => updateStarred(email)}
+                />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </div>
+          );
+        })}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
