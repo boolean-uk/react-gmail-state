474f27bf4fb2d09b9d7087bf815cef9a227c1a5f
skjalg <spectraldesign1@gmail.com>
2024-02-26 09:41:31 +0100

    Core

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Header from './components/Header'
 import initialEmails from './data/emails'
-
+import { useState } from 'react'
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails)
 
   return (
     <div className="app">
@@ -14,32 +14,58 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+            onClick={() => { }}
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+            onClick={() => { }}
           >
             <span className="label">Starred</span>
             <span className="count">?</span>
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+              onChange={() => { }}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
-    </div>
+      <main className="emails">
+        {
+          /* Render a list of emails here */
+          emails.map((email, _) => (
+            < li key={email.id} className={`email ${email.read ? "read" : "unread"}`} >
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                />
+              </div>
+              <div className="sender">
+                {email.sender}
+              </div>
+              <div className="title">
+                {email.title}
+              </div>
+            </li>
+          ))
+        }
+      </main>
+    </div >
   )
 }
 
