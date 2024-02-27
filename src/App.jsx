43efd7215422a504c71973797b5ce32906080eaa
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-02-27 16:00:55 +0100

    Misc: Populated Email List and Added Ability to Toggle Starred

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import initialEmails from './data/emails'
 
 import './styles/App.css'
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails)
+  const [displayedEmails, setDisplayedEmails] = useState(initialEmails)
+
+  const toggleRead = (email) => {
+    console.log(email.title)
+    
+  }
+
+  const toggleStarred = (targetEmail) => {
+    setEmails(emails.map((email) =>
+    email.id === targetEmail.id ? {...email, starred: !email.starred} : email))
+  }
+
+  useEffect(() => {
+    let filteredList = emails
+    setDisplayedEmails(filteredList)
+  }, [emails])
 
   return (
     <div className="app">
@@ -38,7 +54,31 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul>
+          {displayedEmails.map((email, index) => (
+            <li className="email" key={index}>
+            <div className="select">
+            <input
+              className="select-checkbox"
+              type="checkbox"
+              checked={email.read}
+              onChange={() => toggleRead(email)}/>
+            </div>
+            <div className="star">
+            <input
+              className="star-checkbox"
+              type="checkbox"
+              checked={email.starred}
+              onChange={() => toggleStarred(email)}
+            />
+            </div>
+            <div className="sender">{email.sender}</div>
+            <div className="title">{email.title}</div>
+          </li>
+          ))}
+        </ul>
+      </main>
     </div>
   )
 }
