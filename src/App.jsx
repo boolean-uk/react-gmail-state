8b9e3bb0105ac5ad084cd685df3c6e9564054077
Giar Rehani <giarreh1@gmail.com>
2024-02-28 10:34:31 +0100

    User can now toggle star and toggle read

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import initialEmails from './data/emails'
 
@@ -5,8 +6,25 @@ import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails)
 
+  const toggleRead = (targetEmail) => {
+    const updatedEmails = emails =>
+    emails.map(email =>
+        email.id === targetEmail.id ? { ...email, read: !email.read } : email
+      )
+    setEmails(updatedEmails)
+  }
+
+  const toggleStar = (targetEmail) => {
+    const updatedEmails = emails.map(email =>
+      email.id === targetEmail.id ? { ...email, starred: !email.starred } : email
+    )
+    setEmails(updatedEmails)
+
+
+  } 
+  
   return (
     <div className="app">
       <Header />
@@ -38,7 +56,33 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{
+        <ul>
+          {emails.map((email, index) => (
+            <li
+              key={index}
+              className={`email ${email.read ? 'read' : 'unread'}`}
+              // onClick={() => {}}
+            >
+              <div className="select">
+                <input className='select-checkbox'
+                type="checkbox"
+                checked={email.read}
+                onChange={() => toggleRead(email)}></input>
+              </div>
+              <div className="star">
+                <input className='star-checkbox'
+                type="checkbox"
+                checked={email.starred}
+                onChange={() => toggleStar(email)}></input>
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+        </ul>
+        }
+        </main>
     </div>
   )
 }
