2ef2c7b26a5582f62a4ba90e1daff6f5ed46d7d7
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-02-28 13:04:08 +0100

    Renders initial list of emails

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
 import initialEmails from './data/emails'
 
@@ -5,7 +7,7 @@ import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails)
 
   return (
     <div className="app">
@@ -38,7 +40,30 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{emails.map((email) => (
+      <>
+        <li className=
+        {`email ${email.read ? 'read' : 'unread'}`}
+        > 
+          <div className="select">
+          <input
+            className="select-checkbox"
+            type="checkbox"
+            checked={email.read}/>
+          </div>
+          <div className="star">
+          <input
+            className="star-checkbox"
+            type="checkbox"
+            checked={email.starred}
+          />
+          </div>
+          <div className="sender">{email.sender}</div>
+          <div className="title">{email.title}</div>
+        </li>
+      </>
+      )
+      )}</main>
     </div>
   )
 }
