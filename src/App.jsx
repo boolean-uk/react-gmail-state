0a83ad016609f242f339be210f6494f03fb6bf09
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-02-28 09:19:42 +0100

    App.jsx: Made Left-Menu Functional

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,16 @@ function App() {
   const [emails, setEmails] = useState(initialEmails)
   const [displayedEmails, setDisplayedEmails] = useState(initialEmails)
   const [hideRead, setHideRead] = useState(false)
-  
+  const [currentTab, setCurrentTab] = useState("all")
+  const [inboxInfo, setInboxInfo] = useState({})
+
+  const updateInboxInfo = () => {
+    const info = {}
+    info.numUnreadEmails = emails.filter(email => !email.read).length
+    info.numStarredEmails = emails.filter(email => email.starred).length
+    return setInboxInfo(info)
+  }
+
   const toggleRead = (targetEmail) => {
     setEmails(emails.map((email) =>
     email.id === targetEmail.id ? {...email, read: !email.read} : email))
@@ -20,12 +29,16 @@ function App() {
   }
 
   useEffect(() => {
+    updateInboxInfo()
     let filteredEmails = emails
     if (hideRead) {
       filteredEmails = filteredEmails.filter(email => !email.read)
     }
+    if (currentTab === "starred") {
+      filteredEmails = filteredEmails.filter(email => email.starred)
+    }
     setDisplayedEmails(filteredEmails)
-  }, [emails, hideRead])
+  }, [emails, hideRead, currentTab])
 
   return (
     <div className="app">
@@ -33,18 +46,18 @@ function App() {
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
-            className="item active"
-            // onClick={() => {}}
+            className={currentTab === "all" ? "item active" : "item"}
+            onClick={()=> setCurrentTab("all")}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{inboxInfo.numUnreadEmails}</span>
           </li>
           <li
-            className="item"
-            // onClick={() => {}}
+            className={currentTab === "starred" ? "item active" : "item"}
+            onClick={()=> setCurrentTab("starred")}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{inboxInfo.numStarredEmails}</span>
           </li>
 
           <li className="item toggle">
